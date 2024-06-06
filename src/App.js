7a427afc79a86d44e29ae1dda2328b649fcb3655
stefan4012it <stefanra555@gmail.com>
2024-06-06 13:57:30 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import FoodService from './pages/FoodService';
 function App() {
   return (
     <Router>
-      <Routes>
+      <Routes basename={process.env.PUBLIC_URL}>
 
         <Route path="/" element={<Home />} />
         <Route path="/entertainment-vending" element={<EntertainmentVending />} />
