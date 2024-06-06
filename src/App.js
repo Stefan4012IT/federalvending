09089f30cb8b6711244d0fb10d22a7bb809f6fb5
stefan4012it <stefanra555@gmail.com>
2024-06-06 14:19:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import './assets/styles/main.scss';
 import EntertainmentVending from './pages/EntertainmentVending';
